a44428ecec82c6c81e22fda28d5be318bec6250b
tronborg2000 <colinweldon1980@gmail.com>
2022-12-16 09:19:18 +0000

    add grid and guess code

--- run.py
+++ run.py
@@ -1,4 +1,31 @@
 import random 
 
 #make the 5x5 grid
-grid = [[''for_in range(5)]for_in range (5)]
+grid = [[' ' for _ in range(5)] for _ in range (5)]
+
+#main game loop
+
+while True:
+    x = random.randint(0, 4)
+    y = random.randint(0, 4)
+    grid[x][y] = 'X'
+
+#print the grid
+    for row in grid
+    print(''join (row))
+
+# Get the player's guess
+  x = int(input("Enter the x coordinate of your guess: "))
+  y = int(input("Enter the y coordinate of your guess: "))
+
+  # Check if the guess is a hit or a miss
+  if grid[x][y] == 'B':
+    print("Hit!")
+    grid[x][y] = 'X'
+  else:
+    print("Miss.")
+
+  # Check if the player has won
+  if not any('B' in row for row in grid):
+    print("You won!")
+    break
