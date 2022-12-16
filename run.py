85ca465ec1fefddfeb14e6001b4413607b93b12e
tronborg2000 <colinweldon1980@gmail.com>
2022-12-16 09:04:54 +0000

    add rows

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+rows = 0
+columns = 0
+board = []
+ship_row = 0
+ship_column = 0
+turns = 0
+
+
+"""
+Welcome message
+"""
+print("Welcome to battleships!")
+
+
+"""
+Request user to select number of rows
+"""
+while True:
+    try:
+        rows = int(input("Please enter the number of rows (4-9).\n"))
+    except ValueError:
+        print("That is not a number! Try again")
+    else:
+        if 4 <= rows < 10:
+            break
+        else:
+            print("Invalid number, please try again.")
